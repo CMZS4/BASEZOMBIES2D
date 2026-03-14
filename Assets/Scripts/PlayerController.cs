81936aa40de6e39934573efbc004f44c6e1eb146
CMZS4 <salihsahinerr4@hotmail.com>
2026-03-14 23:17:20 +0300

    Add all weapons including shotgun with pellet system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,14 +33,17 @@ public class PlayerController : MonoBehaviour
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
-        // Otomatik ateş - basılı tut
         if (Input.GetMouseButton(0) && !ClaimUI.panelAcik)
         {
             if (Time.time - lastFireTime >= 1f / weaponSystem.ActiveWeapon.fireRate)
             {
                 if (weaponSystem.CanShoot())
                 {
-                    Shoot(lookDir);
+                    if (weaponSystem.ActiveWeapon.isShotgun)
+                        ShootShotgun(lookDir);
+                    else
+                        Shoot(lookDir);
+
                     weaponSystem.UseAmmo();
                     lastFireTime = Time.time;
                 }
@@ -51,7 +54,6 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // R ile doldur
         if (Input.GetKeyDown(KeyCode.R))
             weaponSystem.Reload();
     }
@@ -68,12 +70,35 @@ public class PlayerController : MonoBehaviour
         bullet.GetComponent<Bullet>().damage = weaponSystem.ActiveWeapon.damage;
     }
 
+    void ShootShotgun(Vector2 direction)
+    {
+        int pelletCount = 5;
+        float spreadAngle = 30f;
+
+        for (int i = 0; i < pelletCount; i++)
+        {
+            float angle = -spreadAngle / 2 + (spreadAngle / (pelletCount - 1)) * i;
+            Vector2 pelletDir = RotateVector(direction, angle);
+
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            bullet.GetComponent<Bullet>().SetDirection(pelletDir);
+            bullet.GetComponent<Bullet>().damage = weaponSystem.ActiveWeapon.damage;
+        }
+    }
+
+    Vector2 RotateVector(Vector2 v, float degrees)
+    {
+        float rad = degrees * Mathf.Deg2Rad;
+        return new Vector2(
+            v.x * Mathf.Cos(rad) - v.y * Mathf.Sin(rad),
+            v.x * Mathf.Sin(rad) + v.y * Mathf.Cos(rad)
+        );
+    }
+
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        Debug.Log("Player can: " + currentHealth + "/" + maxHealth);
-        if (currentHealth <= 0)
-            Die();
+        if (currentHealth <= 0) Die();
     }
 
     public void Die()
--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -5,12 +5,13 @@ public class Weapon
 {
     public string weaponName;
     public int damage;
-    public float fireRate;      // saniyede kaç mermi
+    public float fireRate;
     public int maxAmmo;
     public int currentAmmo;
-    public float dropRateBonus; // token drop bonusu
+    public float dropRateBonus;
+    public bool isShotgun;
 
-    public Weapon(string name, int dmg, float rate, int ammo, float dropBonus)
+    public Weapon(string name, int dmg, float rate, int ammo, float dropBonus, bool shotgun = false)
     {
         weaponName = name;
         damage = dmg;
@@ -18,27 +19,49 @@ public class Weapon
         maxAmmo = ammo;
         currentAmmo = ammo;
         dropRateBonus = dropBonus;
+        isShotgun = shotgun;
     }
 }
 
 public class WeaponSystem : MonoBehaviour
 {
-    public Weapon[] weapons = new Weapon[]
-    {
-        new Weapon("Pistol", 1, 2f, 12, 0f),
-        new Weapon("AK47",   3, 5f, 30, 0.05f),
-        new Weapon("MP5",    2, 8f, 25, 0.02f),
-        new Weapon("M249",   4, 10f, 100, 0.08f),
-    };
-
     public int activeWeaponIndex = 0;
-    public int secondWeaponIndex = -1; // -1 = boş
+    public int secondWeaponIndex = -1;
+
+    private Weapon[] _weapons;
+
+    public Weapon[] weapons
+    {
+        get
+        {
+            if (_weapons == null || _weapons.Length == 0)
+                InitWeapons();
+            return _weapons;
+        }
+    }
+
+    void Awake()
+    {
+        InitWeapons();
+    }
+
+    void InitWeapons()
+    {
+        _weapons = new Weapon[]
+        {
+            new Weapon("Pistol",  1, 2f,   12,  0f),
+            new Weapon("Glock",   1, 3.5f, 20,  0f),
+            new Weapon("MP5",     2, 8f,   25,  0f),
+            new Weapon("AK47",    3, 5f,   30,  0f),
+            new Weapon("Shotgun", 5, 1f,   6,   0f, true),
+            new Weapon("M249",    4, 10f,  100, 0f),
+        };
+    }
 
     public Weapon ActiveWeapon => weapons[activeWeaponIndex];
 
     void Update()
     {
-        // Q ile silah değiştir
         if (Input.GetKeyDown(KeyCode.Q))
             SwitchWeapon();
     }
@@ -46,11 +69,9 @@ public class WeaponSystem : MonoBehaviour
     public void SwitchWeapon()
     {
         if (secondWeaponIndex == -1) return;
-
         int temp = activeWeaponIndex;
         activeWeaponIndex = secondWeaponIndex;
         secondWeaponIndex = temp;
-
         Debug.Log("Silah değişti: " + ActiveWeapon.weaponName);
     }
 
